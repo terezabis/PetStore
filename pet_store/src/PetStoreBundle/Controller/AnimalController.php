4d1a0037bc70dacdb262d719b907d8f9e393693b
Tereza Biserina <tbiserina@gmail.com>
2018-12-22 15:05:13 +0200

    User can delete product from the store. Closes #3

--- pet_store/src/PetStoreBundle/Controller/AnimalController.php
+++ pet_store/src/PetStoreBundle/Controller/AnimalController.php
@@ -11,32 +11,31 @@ use PetStoreBundle\Form\AnimalType;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 
-class AnimalController extends Controller 
-{
+class AnimalController extends Controller {
+
     /**
      * @Route("/animal/create", name="animal_create")
      * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function createAction(Request $request)
+    public function createAction(Request $request) 
     {
         $animal = new Animal();
         $form = $this->createForm(AnimalType::class, $animal);
         $form->handleRequest($request);
-            
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();        
+            $em = $this->getDoctrine()->getManager();
             $em->persist($animal);
             $em->flush();
 
             return $this->redirectToRoute("homepage");
         }
 
-        return $this->render('animal/create.html.twig',
-            ['form' => $form->createView()]);
+        return $this->render('animal/create.html.twig', ['form' => $form->createView()]);
     }
-    
+
     /**
      * @Route("/animal/edit/{id}", name="animal_edit")
      * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
@@ -47,9 +46,9 @@ class AnimalController extends Controller
     public function editAction(Request $request, $id)
     {
         $animal = $this
-            ->getDoctrine()
-            ->getRepository(Animal::class)
-            ->find($id);
+                ->getDoctrine()
+                ->getRepository(Animal::class)
+                ->find($id);
 
         if ($animal === null) {
             return $this->redirectToRoute("homepage");
@@ -66,8 +65,33 @@ class AnimalController extends Controller
             return $this->redirectToRoute("homepage");
         }
 
-        return $this->render('animal/edit.html.twig',
-            ['form' => $form->createView(),
-                'animal' => $animal]);
+        return $this->render('animal/edit.html.twig', ['form' => $form->createView(),
+                    'animal' => $animal]);
     }
+
+    /**
+     * @Route("/animal/delete/{id}", name="animal_delete")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     * @param Request $request
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAction(Request $request, $id) 
+    {
+        $animal = $this
+                ->getDoctrine()
+                ->getRepository(Animal::class)
+                ->find($id);
+
+        if ($animal === null) {
+            return $this->redirectToRoute("homepage");
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($animal);
+        $em->flush();
+
+        return $this->redirectToRoute("homepage");
+    }
+
 }
